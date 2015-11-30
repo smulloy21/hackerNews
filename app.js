326bf66a9a4c5c7ba93d656df76e34f86ac753ad
Shannon Mulloy <student@epicodus.com>
2015-11-30 11:47:09 -0800

    add ability to upvote

--- app.js
+++ app.js
@@ -11,4 +11,12 @@ function($scope){
     {title: 'post 4', upvotes: 9},
     {title: 'post 5', upvotes: 4}
   ];
+  $scope.addPost = function(){
+    if(!$scope.title || $scope.title === '') { return; }
+    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.title = '';
+  };
+  $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
 }]);
