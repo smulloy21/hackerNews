c6d845374caa8fa4d9e3464c9b2ebbb5ff9f6863
Shannon Mulloy <student@epicodus.com>
2015-11-30 13:50:14 -0800

    add ui-router with inline template

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var app = angular.module('hackerNews', []);
+var app = angular.module('hackerNews', ['ui.router']);
+
 app.factory('posts', [function(){
   var o = {
     posts: [
@@ -11,22 +12,40 @@ app.factory('posts', [function(){
   };
   return o;
 }])
+
 app.controller('MainCtrl', [
-'$scope',
-'posts',
-function($scope, posts){
-  $scope.posts = posts.posts;
-  $scope.addPost = function(){
-    if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({
-      title: $scope.title,
-      link: $scope.link,
-      upvotes: 0
-    });
-    $scope.title = '';
-    $scope.link = '';
-  };
-  $scope.incrementUpvotes = function(post) {
-    post.upvotes += 1;
-  };
-}]);
+  '$scope',
+  'posts',
+  function($scope, posts){
+    $scope.posts = posts.posts;
+    $scope.addPost = function(){
+      if(!$scope.title || $scope.title === '') { return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    };
+  }
+]);
+
+app.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }
+]);
