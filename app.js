e74afb9aa25f9cc7cd77816e57ebdb940405ebf2
Shannon Mulloy <student@epicodus.com>
2015-11-30 13:29:59 -0800

    add factory / service to hold posts

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
 var app = angular.module('redditClone', []);
-
+app.factory('posts', [function(){
+  var o = {
+    posts: [
+      {title: 'post 1', upvotes: 5},
+      {title: 'post 2', upvotes: 2},
+      {title: 'post 3', upvotes: 15},
+      {title: 'post 4', upvotes: 9},
+      {title: 'post 5', upvotes: 4}
+    ]
+  };
+  return o;
+}])
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+'posts',
+function($scope, posts){
+  $scope.posts = posts.posts;
   $scope.addPost = function(){
     if(!$scope.title || $scope.title === '') { return; }
-    $scope.posts.push({title: $scope.title, upvotes: 0});
+    $scope.posts.push({
+      title: $scope.title,
+      link: $scope.link,
+      upvotes: 0
+    });
     $scope.title = '';
+    $scope.link = '';
   };
   $scope.incrementUpvotes = function(post) {
     post.upvotes += 1;
